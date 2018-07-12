ef4082b1618f93a3c90e7abc1c79fae86b1b94c6
ldilshan <lahirudilshanme@gmail.com>
2018-07-12 12:25:29 +0530

    added two components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { CustomerComponent } from './customer/customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    CustomerComponent
   ],
   imports: [
     BrowserModule
